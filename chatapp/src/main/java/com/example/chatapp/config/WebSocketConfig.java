7f75956537d86abe700b39e38b647b0d2614dc67
A-LPHARM <hemeka2009@gmail.com>
2024-08-29 19:55:41 +0100

    chatapp

--- chatapp/src/main/java/com/example/chatapp/config/WebSocketConfig.java
+++ chatapp/src/main/java/com/example/chatapp/config/WebSocketConfig.java
@@ -3,6 +3,9 @@ package com.example.chatapp.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.*;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
 @EnableWebSocketMessageBroker
